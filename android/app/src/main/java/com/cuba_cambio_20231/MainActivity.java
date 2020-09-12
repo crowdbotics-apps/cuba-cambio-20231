9f796dfe54e9996d4f5798e03cfc6f12741ce2ac
Crowdbotics <team@crowdbotics.com>
2020-09-12 23:03:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cuba_cambio_20231/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cuba_cambio_20231;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "cuba_cambio_20231";
+    }
+}
